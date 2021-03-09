0ba9a2c67a5d214ce28f00e2450a7a138fcfc0c5
Ahmet.kg <bio_ahmet@mail.ru>
2021-03-09 09:20:32 +0600

    some problems fixed

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" app clipped floating temporary>
-      <v-list>
+      <v-list dense>
         <v-row class="ma-2 mb-3">
           <v-icon class="mx-4" large>mdi-video</v-icon>
           <v-toolbar-title class="align-center">
             <span class="title">VideoKg</span>
           </v-toolbar-title>
         </v-row>
+        <v-list-item v-for="item in items" :key="item.icon" link>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar app clipped-left>
@@ -36,7 +46,11 @@
           offset-x
         >
           <template v-slot:activator="{ on }">
-            <v-avatar size="40"><img v-on="on" src="data:user.png"/></v-avatar>
+            <v-avatar size="40"
+              ><img
+                v-on="on"
+                src="https://image.freepik.com/free-vector/illustration-user-avatar-icon_53876-5907.jpg"
+            /></v-avatar>
           </template>
 
           <v-card>
@@ -113,14 +127,21 @@ export default {
   name: "App",
 
   data: () => ({
+    items: [
+      { icon: "mdi-trending-up", text: "Popular Content" },
+      { icon: "mdi-youtube-subscription", text: "Subscribe" },
+      { icon: "mdi-history", text: "History" },
+      { icon: "mdi-playlist-play", text: "More Content" },
+      { icon: "mdi-clock", text: "Watch Later" }
+    ],
     drawer: false,
     menu: false,
-    theme: false,
+    theme: false
   }),
   watch: {
     theme: function(next) {
       this.$vuetify.theme.dark = next;
-    },
-  },
+    }
+  }
 };
 </script>
